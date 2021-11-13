5425d9d5833117266738b6e252dfefcc722dcce6
Asim Thapa <asimtut@gmail.comclear>
2021-11-13 16:50:02 -0500

    Initial commit

---
+++ src/main/java/com/sudocat/budgetreadsdataloader/BudgetReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.sudocat.budgetreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetReadsDataLoaderApplication.class, args);
+	}
+
+}
