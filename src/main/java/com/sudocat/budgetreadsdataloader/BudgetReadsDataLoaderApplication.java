62410a2cbf61c9a8265687f25fe646572ecf7493
asimthapa <asimtut@gmail.com>
2021-12-28 15:28:38 -0500

    Finished adding author

--- src/main/java/com/sudocat/budgetreadsdataloader/BudgetReadsDataLoaderApplication.java
+++ src/main/java/com/sudocat/budgetreadsdataloader/BudgetReadsDataLoaderApplication.java
@@ -1,25 +1,42 @@
 package com.sudocat.budgetreadsdataloader;
 
+import com.sudocat.budgetreadsdataloader.author.Author;
+import com.sudocat.budgetreadsdataloader.author.AuthorRepository;
 import com.sudocat.budgetreadsdataloader.connection.DataStaxAstraProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BudgetReadsDataLoaderApplication {
 
+	@Autowired
+	private AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetReadsDataLoaderApplication.class, args);
 	}
 
+	@PostConstruct
+	public void start() {
+		Author author = new Author();
+		author.setPersonalName("asim");
+		author.setName("asimPen");
+		author.setId("oax");
+		authorRepository.save(author);
+		System.out.println("Finished saving");
+	}
+
 	@Bean
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-		Path bundle =astraProperties.getSecureConnectBundle().toPath();
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 }
